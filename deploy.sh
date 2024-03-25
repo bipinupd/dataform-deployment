c376005ea0e6a26b2f858637da5f2a551dfe7d0f
Bipin Upadhyaya <bipinupd@gmail.com>
2024-03-25 09:36:14 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "{\"projectId\": \"${PROJECT_ID}\", \"location\": \"${BQ_LOCATION}\"}" > .df-credentials.json
+ npm i -g -quiet @dataform/cli
+npm i -g envsub
+envsubst < dataform.json > dataform.json
 dataform install 
 dataform run
