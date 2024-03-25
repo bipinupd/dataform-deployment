534f8816eb86b11de194c197e226f3e1a6745324
Bipin Upadhyaya <bipinupd@gmail.com>
2024-03-25 09:55:29 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 echo "{\"projectId\": \"${PROJECT_ID}\", \"location\": \"${BQ_LOCATION}\"}" > .df-credentials.json
 npm i -g -quiet @dataform/cli
-cat << EOF >> dataform.json
-{
-    "defaultSchema": "example1",
-    "assertionSchema": "dataform_assertions",
-    "warehouse": "bigquery",
-    "defaultDatabase": "$PROJECT_ID",
-    "defaultLocation": "us-central1",
-    "vars": {
-      "STAGING_SCHEMA": "stage",
-      "REPORTING_SCHEMA": "report"
-    }
-  }
-EOF
 dataform install 
 dataform run
